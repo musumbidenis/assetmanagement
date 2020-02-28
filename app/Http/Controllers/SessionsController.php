82f37dcd4d40ac685f9383688cff4ecf2dbe070b
musumbidenis <musumbidenis@gmail.com>
2020-02-28 12:47:15 +0300

    updated the sessions controller

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,7 +26,7 @@ class SessionsController extends Controller
 
         $session = new Session();
         $session->userId = $request->id;
-        $session->assetId = $assetId;
+        $session->assetId = $request->serial;
         $session->labId = $request->lab;
         $session->sessionStart = now();
 
