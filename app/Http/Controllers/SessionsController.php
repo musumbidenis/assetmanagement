14ee9cd212a1b3b735f924d29a70703ff866954d
musumbidenis <musumbidenis@gmail.com>
2020-02-28 13:39:41 +0300

    Sessions controller updated

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,12 +21,9 @@ class SessionsController extends Controller
      */
     public function start(Request $request)
     {
-        $serial = $request->barcode;
-        $assetId = Asset::select('assetId')->where('serialNumber', $serial)->get();
-
         $session = new Session();
         $session->userId = $request->id;
-        $session->assetId = $assetId;
+        $session->assetId = $request->barcode;
         $session->labId = $request->lab;
         $session->sessionStart = now();
 
