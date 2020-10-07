bc4fe05a815a0262b45df87ddc753fdcfc0aa065
musumbidenis <musumbidenis@gmail.com>
2020-10-07 13:37:17 +0300

    Added some session validation

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -15,12 +15,17 @@ class SessionsController extends Controller
      */
     public function start(Request $request)
     {
-        /*Checks if asset exists in database */
+        /*Checks if asset exists in database
+        * && if asset has been signed off
+        */
         $barcode = $request->barcode;
-        $check = Asset::where('serialNumber', $barcode)->count();
+        $checkAsset = Asset::where('serialNumber', $barcode)->count();
+        $checkSession = Session1::where('serialNumber', $barcode)->where('sessionStop', '2000-01-01 00:00:00')->count();
 
-        if ($check == 0){
-           return response()->json('null');
+        if ($checkAsset == 0){
+           return response()->json('asset does not exist');
+        }elseif ($checkSession != 0){
+            return response()->json('asset not signed off');
         }else{ 
         /*Get the lab id for asset */
         $labId = Asset::select('labId')->where('serialNumber', $barcode)->get()->first();
@@ -28,15 +33,12 @@ class SessionsController extends Controller
         $session = new Session1();
         $session->userId = $request->id;
         $session->serialNumber = $barcode;
-        $session->labId = $labId;
+        $session->labId = $labId->labId;
         $session->sessionStart = now();
 
         $session->save();
            return response()->json('success');
         }
-
-        
-
     }
 
     /*POST
@@ -45,16 +47,22 @@ class SessionsController extends Controller
     {    
         $barcode = $request->barcode;
         $id = $request->id;
-        $stop = now();
 
+        $checkSession = Session1::where('serialNumber', $barcode)->where('userId', $id)->where('sessionStop', '2000-01-01 00:00:00')->count();
+
+        if($checkSession == 0){
+            return response()->json('youre not signed in for this asset');
+        }else{
+            
+        $stop = now();
         DB::table('session1s')
                     ->where('userId', $id)
                     ->where('serialNumber', $barcode)
                     ->where('sessionStop', '2000-01-01 00:00:00')
                     ->update(['sessionStop' => $stop]);
         
-        
         return response()->json('success');
+        }
         
     }
 }
